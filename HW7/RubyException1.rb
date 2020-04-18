94887defe5fccc51c50efbcd402f7476776f6a91
YalingZheng <yaling.zheng@gmail.com>
2020-04-18 14:35:28 -0400

    Create RubyException1.rb

---
+++ HW7/RubyException1.rb
@@ -0,0 +1,17 @@
+class Worker
+  def perform
+    begin
+        retries ||= 0
+        puts "try ##{ retries }"
+        response = File.read("user.txt").split
+        if response.success?
+          puts "Ok, we've got items!"
+        else
+          puts "Uh, server responded with #{response.status}"
+        end
+    end
+end
+
+puts "Hello World"
+worker1 = Worker.new
+worker1.perform
