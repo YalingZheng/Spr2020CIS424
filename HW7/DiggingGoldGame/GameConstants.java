4ce98078dc5c3097ddd85bce42ffa3f1dab3093f
YalingZheng <yaling.zheng@gmail.com>
2020-04-18 14:58:38 -0400

    Create GameConstants.java

---
+++ HW7/DiggingGoldGame/GameConstants.java
@@ -0,0 +1,8 @@
+public class GameConstants {
+	// Maximum number of milliseconds a thread should wait before digging gold 	
+	final static int maxWaitTime = 10;
+	// The number of times a GoldDigger digs gold
+	final static int numTimesDiggingGold = 10;
+	// The Quantity of gold that a GoldDigger digs each time
+	final static int QtyGoldDiggedEachTime = 1;
+}
